c052eae8e2877dfa707f426ba792e2f219b217e0
johnHaloWang <johnHaloWang@gmail.com>
2015-08-28 12:13:07 -0700

    Added my project

---
+++ teaApp.3/ToDoItem.swift
@@ -0,0 +1,33 @@
+//
+//  ToDoItem.swift
+//  teaApp.3
+//
+//  Created by john wang on 8/28/15.
+//  Copyright © 2015 john wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToDoItem: NSObject{
+    
+    var itemName: String
+    var completed: Bool
+    //var creationDate: NSDate
+    
+    init(name: String, com: Bool){
+        self.itemName = name
+        self.completed = com
+    }
+    
+    func markAsCompleted(isCompleted: Bool){
+        self.completed = isCompleted
+    }
+    /*
+    func onDate(date: NSDate){
+    creationDate = date
+    }
+    */
+    
+    
+}
---
+++ teaApp.3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teaApp.3
+//
+//  Created by john wang on 8/26/15.
+//  Copyright © 2015 john wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
